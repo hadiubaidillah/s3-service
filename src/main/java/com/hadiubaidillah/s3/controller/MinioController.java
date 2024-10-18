d5a5333c618972df9132f6b1ba02349ec032d7d7
Hadi Ubaidillah <hadiubaidillahx@gmail.com>
2024-10-18 14:23:17 +0700

    initial commit

---
+++ src/main/java/com/hadiubaidillah/s3/controller/MinioController.java
@@ -0,0 +1,57 @@
+package com.hadiubaidillah.s3.controller;
+
+import com.hadiubaidillah.s3.service.MinioService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/files")
+public class MinioController {
+
+    private final MinioService minioService;
+
+    public MinioController(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = minioService.uploadFile(file);
+            return new ResponseEntity<>(fileName, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error uploading file: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) {
+        try {
+            InputStream fileStream = minioService.downloadFile(fileName);
+            byte[] fileContent = fileStream.readAllBytes();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData(fileName, fileName);
+            return new ResponseEntity<>(fileContent, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        try {
+            minioService.deleteFile(fileName);
+            return new ResponseEntity<>("File deleted successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error deleting file: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
---
+++ src/main/java/com/hadiubaidillah/s3/service/MinioService.java
@@ -0,0 +1,57 @@
+package com.hadiubaidillah.s3.service;
+
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@Service
+public class MinioService {
+
+    @Value("${minio.bucket}")
+    private String bucketName;
+
+    private final MinioClient minioClient;
+
+    public MinioService(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
+    public String uploadFile(MultipartFile file) throws Exception {
+        String fileName = file.getOriginalFilename();
+
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .contentType(file.getContentType())
+                        .build()
+        );
+        return fileName;
+    }
+
+    public InputStream downloadFile(String fileName) throws Exception {
+        return minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .build()
+        );
+    }
+
+    public void deleteFile(String fileName) throws Exception {
+        minioClient.removeObject(
+                RemoveObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .build()
+        );
+    }
+
+}
